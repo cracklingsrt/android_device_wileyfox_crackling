db9e8e803a6aaaf4b18fb64c23b685e963983ce7
Ilya Lebedev <ilya.lebedev.2003@mail.ru>
2017-04-27 20:50:45 +0300

    crackling: Final merge from msm8916-common * Fix RIL
    Change-Id: I34dde30268d8e91f0c0a856811a027fec502658f

--- product/qcom-radio.mk
+++ product/qcom-radio.mk
@@ -8,9 +8,23 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.data.qmi.adb_logmask=0 \
     ro.telephony.default_network=9,9 \
     ril.ecclist=000,08,100,101,102,110,112,118,119,120,122,911,999 \
-    ril.ecclist1=000,08,100,101,102,110,112,118,119,120,122,911,999
+    ril.ecclist1=000,08,100,101,102,110,112,118,119,120,122,911,999 \
+    persist.radio.apm_sim_not_pwdn=1 \
+    ro.telephony.call_ring.multiple=false \
+    ro.use_data_netmgrd=true
 
 # RIL
+PRODUCT_PACKAGES += \
+    libcnefeatureconfig \
+    librmnetctl \
+    libxml2
+
+# Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+    frameworks/native/data/etc/android.hardware.telephony.cdma.xml:system/etc/permissions/android.hardware.telephony.cdma.xml \
+    frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
+
 ifeq ($(QCPATH),)
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/data/netmgr_config.xml:system/etc/data/netmgr_config.xml \
