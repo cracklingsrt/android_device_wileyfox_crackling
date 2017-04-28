75cd9fb3cfcb84d7c61105624d784b890b04bebb
Ilya Lebedev <ilya.lebedev.2003@mail.ru>
2017-04-28 12:43:40 +0300

    crackling: Add dex2oat setting from stock * Some merge from TOS093F - NMF26F stock image
    Change-Id: I3fa425691f5afa7239b00f3e0c300e256b425fe5

--- product/perf.mk
+++ product/perf.mk
@@ -2,3 +2,7 @@
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.am.reschedule_service=true \
     ro.config.max_starting_bg=8
+
+# Optimal dex2oat threads for faster app installation
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sys.fw.dex2oat_thread_count=4
