8df50333fd69c9c0ad755bb9b827c6ac8c44bd01
Ilya Lebedev <ilya.lebedev.2003@mail.ru>
2017-04-30 08:34:33 +0300

    crackling: For AOSCP
    Change-Id: Idb978579e86c45a01e362ddc1892d2637458ab93

--- doze/Android.mk
+++ doze/Android.mk
@@ -8,14 +8,6 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_PACKAGE_NAME := CracklingDoze
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
-LOCAL_STATIC_JAVA_LIBRARIES := \
-    android-support-v4 \
-    android-support-v13 \
-    android-support-v7-recyclerview \
-    android-support-v7-preference \
-    android-support-v7-appcompat \
-    android-support-v14-preference \
-    org.cyanogenmod.platform.internal
 
 LOCAL_RESOURCE_DIR := \
     $(LOCAL_PATH)/res \
